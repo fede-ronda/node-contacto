944c0960d3cab2b540647fc8e45dcde5ed617112
Federico Rondanini <fede.ronda@gmail.com>
2022-06-16 01:01:37 -0300

    a

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,16 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var usuariosModel = require('../../models/usuariosModel');
+var novedadesModel = require('../../models/novedadesModel');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('admin/novedades',{
+router.get('/', async function(req, res, next) {
+  var novedades = await novedadesModel.getNovedades();
+  res.render('admin/novedades' , {
       layout:'admin/layout',
-      usuario: req.session.nombre //fede
-  });
+      usuario : req.session.nombre,
+      novedades 
+  } );
 });
 
+  
 
-
+router.get('/eliminar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades')
+   
+}); 
 
 module.exports = router;
